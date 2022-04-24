1f3f9f011c58e4fdd61234147ce3d551ce76d5c0
Ing. Ebu Celik <ebucelik1@hotmail.com>
2022-04-24 19:17:37 +0200

    Initial Commit

---
+++ Lawme/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lawme
+//
+//  Created by Ing. Ebu Celik on 24.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lawme/LawmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LawmeApp.swift
+//  Lawme
+//
+//  Created by Ing. Ebu Celik on 24.04.22.
+//
+
+import SwiftUI
+
+@main
+struct LawmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
